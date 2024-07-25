dc779058d8864561ff94e159b10748178ef77844
Marcos Vinicius Santos Nery <marcos.nery@gestao.gov.br>
2024-07-25 10:08:50 -0300

    Alteração entidade merchant

--- app/Entities/Merchant.php
+++ app/Entities/Merchant.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace App\Entities;
 
-use App\Traits\UuidTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class Merchant extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, UuidTrait;
+    use HasApiTokens;
 
     protected $fillable = [
         'name',
